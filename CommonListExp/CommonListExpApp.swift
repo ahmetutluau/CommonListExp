36e2c6e699e716457979fe036a3fd45c487e1b40
Ahmet Utlu <ahmet.utlu@basefy.com>
2024-03-29 11:28:29 +0100

    Initial Commit

---
+++ CommonListExp/CommonListExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonListExpApp.swift
+//  CommonListExp
+//
+//  Created by Ahmet Utlu on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CommonListExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
